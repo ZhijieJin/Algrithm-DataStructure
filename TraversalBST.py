287acbc3207d439037907f8f2fbd09c93c973c78
ZhijieJin <36421589+ZhijieJin@users.noreply.github.com>
2018-02-20 14:54:20 +0800

    Update TraversalBST.py

--- TraversalBST.py
+++ TraversalBST.py
@@ -1,7 +1,13 @@
-#1. Preorder
-#2. Inorder
-#3. Postorder
-#4. Breadth First / Level Order Traversal
+    1
+   / \
+  2   3
+ / \
+4   5
+
+#1. Preorder (root, left, right):  1 2 4 5 3
+#2. Inorder (left, root, right):   4 2 5 1 3
+#3. Postorder (left, right, root): 4 5 2 3 1
+#4. Breadth First / Level Order Traversal (root): 1 2 3 4 5
 
 #1. Preorder with input argument tree
 def preorder(tree):
@@ -18,3 +24,18 @@ def preorder(self):
     self.leftChild.preorder()
   if self.leftRight:
     self.rightChild.preorder()
+    
+#2. Inorder
+def inorder(tree):
+  if tree:
+    inorder(tree.getLeftChild())
+    print(tree.getRootVal())
+    inorder(tree.getRightChild())
+    
+    
+#3. Postorder
+def postorder(tree):
+  if tree:
+    postorder(tree.getLeftChild())
+    postorder(tree.getRightChild())
+    print(tree.getRootVal())
